ffad7666c6f8a93b25e603a9684d2971a454f4f8
Abhishek Muddebihal <abhishek12muddebihal@gmail.com>
2024-05-26 14:44:52 +0000

    Added argument

--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@ from queues.observable_queue import ObservableQueue
 from generators.leads_generator import simulate_lead_generation
 from processors.lead_processor import LeadProcessor
 
+import sys
 
-def main():
+
+def main(num_of_leads):
     print("Initializing LeadQueue...")
     
     leadProcessor = LeadProcessor()
@@ -12,10 +14,18 @@ def main():
     leadQueue.subscribe(leadProcessor.process_lead)
 
     print("Initialization of LeadQueue Completed.")
-    for lead in simulate_lead_generation(20):
+    for lead in simulate_lead_generation(num_of_leads):
         leadQueue.put(lead)
 
 
 
 if __name__ == "__main__":
-    main()
+    
+    if len(sys.argv) != 2:
+        print("Usage : python main.py <number_of_leads_to_generate>")
+        sys.exit(1)
+    
+    # Access the arguments
+    num_of_leads = int(sys.argv[1])
+
+    main(num_of_leads)
